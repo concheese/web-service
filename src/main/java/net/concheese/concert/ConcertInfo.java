14148397d6f82b5d182d47dc84128300506f18f2
Ji Myoung Ha <webmaster@null0xff.com>
2023-08-25 10:13:27 +0900

    :art: 콘서트 모델들에 대하여 lombok 어노테이션 사용

--- src/main/java/net/concheese/concert/ConcertInfo.java
+++ src/main/java/net/concheese/concert/ConcertInfo.java
@@ -1,82 +1,32 @@
 package net.concheese.concert;
 
 import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class ConcertInfo {
-    private long id;
-    private String title;
-    private String genre;
-    private String performers;
-    private List<ConcertTime> time;
-    private List<Ticketing> ticketing;
-    private String description;
 
-    public ConcertInfo(long id, String title, String genre, String location, String performers, List<ConcertTime> time, List<Ticketing> ticketing, String description) {
-        this.id = id;
-        this.title = title;
-        this.genre = genre;
-        this.time = time;
-        this.performers = performers;
-        this.ticketing = ticketing;
-        this.description = description;
-    }
+  private long id;
+  private String title;
+  private String genre;
+  private String performers;
+  private List<ConcertTime> time;
+  private List<Ticketing> ticketing;
+  private String description;
 
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getGenre() {
-        return genre;
-    }
-
-    public void setGenre(String genre) {
-        this.genre = genre;
-    }
-
-    public String getPerformers() {
-        return performers;
-    }
-
-    public void setPerformers(String performers) {
-        this.performers = performers;
-    }
-
-    public List<ConcertTime> getTime() {
-        return time;
-    }
-
-    public void setTime(List<ConcertTime> time) {
-        this.time = time;
-    }
-
-    public List<Ticketing> getTicketing() {
-        return ticketing;
-    }
-
-    public void setTicketing(List<Ticketing> ticketing) {
-        this.ticketing = ticketing;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
+  public ConcertInfo(long id, String title, String genre, String location, String performers,
+      List<ConcertTime> time, List<Ticketing> ticketing, String description) {
+    this.id = id;
+    this.title = title;
+    this.genre = genre;
+    this.time = time;
+    this.performers = performers;
+    this.ticketing = ticketing;
+    this.description = description;
+  }
+  
 }
 
 
--- src/main/java/net/concheese/concert/ConcertTime.java
+++ src/main/java/net/concheese/concert/ConcertTime.java
@@ -1,36 +1,21 @@
 package net.concheese.concert;
 
 import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class ConcertTime {
-    private final Long id;
-    private String location;
-    private LocalDateTime dateTime;
 
-    public Long getId() {
-        return id;
-    }
+  private long id;
+  private String location;
+  private LocalDateTime dateTime;
 
-    public String getLocation() {
-        return location;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
-    }
-
-    public LocalDateTime getDateTime() {
-        return dateTime;
-    }
-
-    public void setDateTime(LocalDateTime dateTime) {
-        this.dateTime = dateTime;
-    }
-
-    public ConcertTime(Long id, String location, LocalDateTime dateTime) {
-        this.id = id;
-        this.location = location;
-        this.dateTime = dateTime;
-    }
+  public ConcertTime(Long id, String location, LocalDateTime dateTime) {
+    this.id = id;
+    this.location = location;
+    this.dateTime = dateTime;
+  }
 
 }
