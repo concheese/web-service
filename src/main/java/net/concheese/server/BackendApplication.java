62a90c95e0eaf7ec5781c32e83354a060feee411
Ji Myoung Ha <webmaster@null0xff.com>
2023-10-21 01:18:29 +0900

    :memo: JavaDoc 수정

--- src/main/java/net/concheese/server/BackendApplication.java
+++ src/main/java/net/concheese/server/BackendApplication.java
@@ -2,9 +2,10 @@ package net.concheese.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
- * 'concheese-backend' 스프링 부트 애플리케이션의 진입점입니다.
+ * <strong>콘치즈</strong> 스프링 부트 애플리케이션의 진입점입니다.
  *
  * <p>이 클래스는 애플리케이션의 시작점으로 작용합니다. 스프링 부트 애플리케이션 컨텍스트를 초기화하고 설정하며, 애플리케이션을 실행시킵니다.
  * {@link #main(String[])} 메서드는 실행의 시작점입니다.
@@ -13,6 +14,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @since 2023-08-04
  */
 @SpringBootApplication
+@EnableJpaRepositories
 public class BackendApplication {
 
   /**
--- src/main/java/net/concheese/server/ServletInitializer.java
+++ src/main/java/net/concheese/server/ServletInitializer.java
@@ -4,7 +4,8 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
- * 'concheese-backend' 스프링 부트 애플리케이션을 'war' 파일로 패키징하기 위한 초기화 클래스입니다.
+ * <strong>콘치즈</strong> 스프링 부트 애플리케이션을 <strong>Web Application aRchive</strong>' 파일로 패키징하기 위한 초기화
+ * 클래스입니다.
  *
  * <p>이 클래스는 'war' 파일 형식으로 애플리케이션을 배포할 때 필요한 설정을 정의합니다.
  * {@link #configure(SpringApplicationBuilder)} 메서드는 'war' 파일 형식으로 배포할 때 애플리케이션을 설정하는 역할을 합니다.
@@ -15,7 +16,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 public class ServletInitializer extends SpringBootServletInitializer {
 
   /**
-   * 'war' 파일로 배포할 때 애플리케이션을 설정하는 메서드입니다.
+   * <strong>WAR</strong> 파일로 배포할 때 애플리케이션을 설정하는 메서드입니다.
    *
    * @param application 스프링 애플리케이션 빌더 객체입니다.
    * @return 설정된 스프링 애플리케이션 빌더 객체를 반환합니다.
