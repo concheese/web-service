04cf6094274a0bb03964de9c315c8809387008e7
Myoung Ha, Ji <webmaster@null0xff.com>
2023-10-26 22:28:00 +0900

    :recycle: Concert 도메인 컨트롤러 리팩터링 수행

--- src/main/java/net/concheese/server/info/controller/ConcertInfoRestController.java
+++ src/main/java/net/concheese/server/info/controller/ConcertInfoRestController.java
@@ -1,104 +1,106 @@
 package net.concheese.server.info.controller;
 
 import java.util.List;
-import java.util.Optional;
-import lombok.RequiredArgsConstructor;
+import java.util.UUID;
+import net.concheese.server.info.dto.ConcertForm;
 import net.concheese.server.info.model.Concert;
-import net.concheese.server.concert.model.Type;
+import net.concheese.server.info.model.ConcertType;
 import net.concheese.server.info.service.ConcertInfoService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
- * {@code ConcertInfoRestController}는 콘서트 정보 관련 HTTP 요청을 처리하는 컨트롤러입니다.
+ * {@code ConcertInfoRestController} 클래스는 콘서트 정보에 관한 RESTful API를 제공합니다.
+ * <p>
+ * 이 컨트롤러는 콘서트의 등록, 업데이트, 목록 조회, 개별 콘서트의 정보 조회 및 삭제와 같은 엔드포인트를 제공합니다. 각 메서드는
+ * {@code ConcertInfoService}를 사용하여 기본 로직을 처리합니다.
+ * </p>
+ * <p>
+ * API의 기본 경로는 "/api/v1/info/concert"로 설정되어 있습니다.
+ * </p>
  *
- * @version 1.0
+ * @author Lynn Choi
+ * @author MyoungHa Ji
  * @since 2023-09-16
  */
 @RestController
-@RequestMapping(value = "/api/v1/concert")
-@RequiredArgsConstructor
+@RequestMapping(value = "/api/v1/info/concert")
 public class ConcertInfoRestController {
-  @Autowired
+
   private final ConcertInfoService concertInfoService;
 
-  /**
-   * 콘서트 정보를 생성합니다.
-   *
-   * @param request 생성할 콘서트 정보의 요청 데이터.
-   * @return 생성된 콘서트 정보를 담은 ResponseEntity.
-   */
-  @PostMapping("/info")
-  public ResponseEntity<Concert> createInfo(@RequestBody ConcertInfoRequest request) {
-    return ResponseEntity.ok(
-        concertInfoService.createInfo(request.title(), request.type(), request.performers(), request.schedule(), request.ticketing(),
-            request.description(), request.link()));
-  }
-
-  @PostMapping("/echo")
-  public ResponseEntity<String> infoString(@RequestBody ConcertInfoRequest request) {
-    return ResponseEntity.ok(request.toString());
-  }
-
-
-  /**
-   * 콘서트 정보를 업데이트합니다.
-   *
-   * @param request 콘서트 정보 업데이트를 위한 요청 데이터.
-   * @param infoId  업데이트할 콘서트 정보의 식별자.
-   * @return 업데이트된 콘서트 정보를 담은 ResponseEntity.
-   */
-  @PutMapping("/info/{infoId}")
-  public ResponseEntity<Concert> updateInfo(@RequestBody ConcertInfoRequest request,
-                                            @PathVariable long infoId) {
-    return ResponseEntity.ok(
-            concertInfoService.updateInfo(infoId, request.title(), request.type(), request.performers(), request.schedule(), request.ticketing(),
-                    request.description(), request.link()));
+  public ConcertInfoRestController(ConcertInfoService concertInfoService) {
+    this.concertInfoService = concertInfoService;
   }
 
   /**
-   * 특정 콘서트 정보를 조회합니다.
+   * 새로운 콘서트를 등록합니다.
    *
-   * @param infoId 조회할 콘서트 정보의 식별자.
-   * @return 조회된 콘서트 정보를 담은 ResponseEntity.
+   * @param request 콘서트 등록 정보를 담은 요청 객체.
+   * @return 등록된 콘서트 객체.
    */
-  @GetMapping("/info/{infoId}")
-  public ResponseEntity<Concert> readInfo(@PathVariable long infoId) {
-    return ResponseEntity.ok(concertInfoService.readInfo(infoId).orElse(null));
+  @RequestMapping("/register")
+  public Concert newConcert(@RequestBody ConcertForm request) {
+    return concertInfoService.saveOrUpdateConcertForm(request);
   }
 
   /**
-   * 콘서트 정보 목록을 조회합니다.
+   * 기존 콘서트 정보를 업데이트합니다.
    *
-   * @param genre 필터링할 콘서트 장르 (옵션).
-   * @return 조회된 콘서트 정보 목록을 담은 ResponseEntity.
+   * @param request 콘서트 업데이트 정보를 담은 요청 객체.
+   * @return 업데이트된 콘서트 객체.
    */
-  @GetMapping("/infoListByGenre")
-  public ResponseEntity<List<Concert>> readInfoListByGenre(@RequestParam Optional<Type> genre) {
-    return ResponseEntity.ok(concertInfoService.readInfoListByGenre(genre.orElse(null)));
+  @RequestMapping("/update")
+  public Concert updateConcert(@RequestBody ConcertForm request) {
+    return concertInfoService.saveOrUpdateConcertForm(request);
   }
 
-  @GetMapping("/infoListByPerformer")
-  public ResponseEntity<List<Concert>> readInfoListByPerformer(@RequestParam Optional<String> performerName){
-    return ResponseEntity.ok(concertInfoService.readInfoListByPerformer(performerName.orElse(null)));
-  }
-
-  // 공연날짜, 공연장 별로 필터링 하는 기능 추가해보기.
-
-  @GetMapping("/readAllInfo")
-    public ResponseEntity<List<Concert>> readAllInfo(){
-        return ResponseEntity.ok(concertInfoService.readAllInfo());
+  /**
+   * 조건에 맞는 콘서트 목록을 반환합니다. 제목, 유형 또는 연주자 이름별로 필터링 할 수 있습니다.
+   *
+   * @param title         제목을 기준으로 필터링 할 경우 사용.
+   * @param type          유형을 기준으로 필터링 할 경우 사용.
+   * @param performerName 연주자 이름을 기준으로 필터링 할 경우 사용.
+   * @return 조건에 맞는 콘서트 목록.
+   */
+  @RequestMapping("/list")
+  public List<Concert> listConcerts(@RequestParam(value = "title", required = false) String title,
+      @RequestParam(value = "type", required = false) String type,
+      @RequestParam(value = "performer", required = false) String performerName) {
+    List<Concert> concerts = concertInfoService.listAll();
+    if (title != null) {
+      concerts.retainAll(concertInfoService.listAllByTitle(title));
     }
+    if (type != null) {
+      concerts.retainAll(concertInfoService.listAllByType(ConcertType.valueOf(type)));
+    }
+    if (performerName != null) {
+      concerts.retainAll(concertInfoService.listAllByPerformerName(performerName));
+    }
+    return concerts;
+  }
+
   /**
-   * 콘서트 정보를 삭제합니다.
+   * 고유 식별자로 콘서트 정보를 반환합니다.
    *
-   * @param infoId 삭제할 콘서트 정보의 식별자.
-   * @return 삭제 성공 여부를 나타내는 ResponseEntity.
+   * @param id 조회하려는 콘서트의 UUID 문자열.
+   * @return 해당 식별자를 가진 콘서트 객체.
    */
-  @DeleteMapping("/info/{infoId}")
-  public ResponseEntity<Void> deleteInfo(@PathVariable long infoId) {
-    concertInfoService.deleteInfo(infoId);
-    return ResponseEntity.ok().build();
+  @RequestMapping("/{id}")
+  public Concert getConcert(@PathVariable String id) {
+    return concertInfoService.getById(UUID.fromString(id));
+  }
+
+  /**
+   * 고유 식별자로 콘서트를 삭제합니다.
+   *
+   * @param id 삭제하려는 콘서트의 UUID 문자열.
+   */
+  @RequestMapping("/delete/{id}")
+  public void deleteConcert(@PathVariable String id) {
+    concertInfoService.delete(UUID.fromString(id));
   }
 }
