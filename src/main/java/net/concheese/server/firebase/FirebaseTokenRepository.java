816f9fa9485cbc10634975caa56e027ce64d0ed6
Dong young <yrrho2@naver.com>
2023-10-28 06:00:48 +0900

    application 통일.

--- src/main/java/net/concheese/server/firebase/FirebaseTokenRepository.java
+++ src/main/java/net/concheese/server/firebase/FirebaseTokenRepository.java
@@ -4,15 +4,14 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Repository;
 
 import java.io.IOException;
 
-@SpringBootApplication
-public class FirebaseNotificationApplication {
+@Repository
+public class FirebaseTokenRepository {
     @Bean
     FirebaseMessaging firebaseMessaging() throws IOException {
         GoogleCredentials googleCredentials = GoogleCredentials
@@ -21,9 +20,5 @@ public class FirebaseNotificationApplication {
                 .setCredentials(googleCredentials).build();
         FirebaseApp app = FirebaseApp.initializeApp(firebaseOptions, "notify-app");
         return FirebaseMessaging.getInstance(app);
-    }
-
-    public static void main(String[] args) {
-        SpringApplication.run(FirebaseNotificationApplication.class,args);
     }
 }
