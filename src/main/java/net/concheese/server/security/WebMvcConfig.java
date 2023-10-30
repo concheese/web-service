850815bbb22eec213fd2a6bdff4e2256526aa8de
Myoung Ha, Ji <webmaster@null0xff.com>
2023-10-30 14:20:16 +0900

    :lock: 모든 곳에서 접근 허용

--- src/main/java/net/concheese/server/security/WebMvcConfig.java
+++ src/main/java/net/concheese/server/security/WebMvcConfig.java
@@ -11,7 +11,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
   @Override
   public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/**").allowedOrigins("http://localhost:5500")
+    registry.addMapping("/**").allowedOrigins("*")
         .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE");
   }
 }
