68ff6225e6d1a5850e555c93ade7cd94a4c0bf1f
Myoung Ha, Ji <webmaster@null0xff.com>
2023-10-31 03:07:58 +0900

    :bug: JWT 값 전달 방식 변경

--- src/main/java/net/concheese/server/user/oauth/OAuth2AuthenticationSuccessHandler.java
+++ src/main/java/net/concheese/server/user/oauth/OAuth2AuthenticationSuccessHandler.java
@@ -39,7 +39,7 @@ public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationS
 
     String jwtToken = jwtTokenUtil.generateToken(user);
 
-    String url = UriComponentsBuilder.fromUriString("https://concheese.net/" + jwtToken)
+    String url = UriComponentsBuilder.fromUriString("https://concheese.net/?jwt=" + jwtToken)
         .build().toUriString();
     //클라이언트 리다이렉트 주소 -> 로그인 성공 후 페이지
 
